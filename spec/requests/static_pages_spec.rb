6751c043d0c3d04bc163fbb9eda08dbe9187c6ed
Michael <mike.ocheltree@plastictrophy.com>
2012-08-10 12:57:28 -0700

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,15 +32,31 @@ describe "Static Pages" do
 
   describe "Home page" do
 	 
-   before { visit root_path }
+    before { visit root_path }
    
-   let(:heading)   { 'Sample App' }
-	 let(:page_title) { '' }
+    let(:heading)   { 'Sample App' }
+	  let(:page_title) { '' }
 
-   it_should_behave_like "all static pages"
-   it { should_not have_selector 'title', text: ' | Home' }		
-		
-  end
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: ' | Home' }	
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
+    
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
+  end	
+end
 
 	describe "Help page" do
 		
